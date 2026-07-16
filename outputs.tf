a16b0e1b2abb046db36846fe83f8bce1b5bca37e
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:48:02 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,45 @@
 output "eventhubs_id" {
   description = "Map of id values across all eventhubs, keyed the same as var.eventhubs"
-  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.id }
+  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "eventhubs_capture_description" {
   description = "Map of capture_description values across all eventhubs, keyed the same as var.eventhubs"
-  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.capture_description }
+  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.capture_description if v.capture_description != null && length(v.capture_description) > 0 }
 }
 output "eventhubs_message_retention" {
   description = "Map of message_retention values across all eventhubs, keyed the same as var.eventhubs"
-  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.message_retention }
+  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.message_retention if v.message_retention != null }
 }
 output "eventhubs_name" {
   description = "Map of name values across all eventhubs, keyed the same as var.eventhubs"
-  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.name }
+  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "eventhubs_namespace_id" {
   description = "Map of namespace_id values across all eventhubs, keyed the same as var.eventhubs"
-  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.namespace_id }
+  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.namespace_id if v.namespace_id != null && length(v.namespace_id) > 0 }
 }
 output "eventhubs_namespace_name" {
   description = "Map of namespace_name values across all eventhubs, keyed the same as var.eventhubs"
-  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.namespace_name }
+  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.namespace_name if v.namespace_name != null && length(v.namespace_name) > 0 }
 }
 output "eventhubs_partition_count" {
   description = "Map of partition_count values across all eventhubs, keyed the same as var.eventhubs"
-  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.partition_count }
+  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.partition_count if v.partition_count != null }
 }
 output "eventhubs_partition_ids" {
   description = "Map of partition_ids values across all eventhubs, keyed the same as var.eventhubs"
-  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.partition_ids }
+  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.partition_ids if v.partition_ids != null && length(v.partition_ids) > 0 }
 }
 output "eventhubs_resource_group_name" {
   description = "Map of resource_group_name values across all eventhubs, keyed the same as var.eventhubs"
-  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.resource_group_name }
+  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "eventhubs_retention_description" {
   description = "Map of retention_description values across all eventhubs, keyed the same as var.eventhubs"
-  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.retention_description }
+  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.retention_description if v.retention_description != null && length(v.retention_description) > 0 }
 }
 output "eventhubs_status" {
   description = "Map of status values across all eventhubs, keyed the same as var.eventhubs"
-  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.status }
+  value       = { for k, v in azurerm_eventhub.eventhubs : k => v.status if v.status != null && length(v.status) > 0 }
 }
 
